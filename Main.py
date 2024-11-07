66b1d5973f04683f107f264b8ae541aa0aef1484
XrMob <jeesmon.xr@gmail.com>
2024-11-06 23:51:13 -0500

    Setup Data Collection

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import cv2 as cv
 import numpy as np
 import os
+
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
@@ -48,27 +51,63 @@ def extract_keypoints(results):
     rh = np.array([[result.x, result.y, result.z] for result in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros((21*3))
     return np.concatenate([pose,face,lh,rh])
 
+#Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data')
+
+#Action that we try to detect
+actions = np.array(['hello','thanks','iloveyou'])
+
+#Thirty videos worth of data
+no_sequence = 30
+
+#Videos are going to be 30 frames in length
+sequence_length = 30
+
+for action in actions:
+    for sequence in range(no_sequence):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+
 capture = cv.VideoCapture(0)
 #Set Mediapipe model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while capture.isOpened():
+    for action in actions:
+        for sequence in range(no_sequence):
+            for frame_num in range(sequence_length):
 
-        #Getting feed from camera
-        retBool, frame = capture.read()
+                #Getting feed from camera
+                retBool, frame = capture.read()
 
-        #Make detection
-        image, results = mediapipe_dectection(frame,holistic)
-        print(results)
+                #Make detection
+                image, results = mediapipe_dectection(frame,holistic)
+                print(results)
 
-        #Draw Landmarks
-        render_formatted_landmarks(image,results)
+                #Draw Landmarks
+                render_formatted_landmarks(image,results)
 
-        #Play video frame
-        cv.imshow('Feed', image)
+                #Apply wait logic
+                if frame_num == 0:
+                    cv.putText(image, 'STARTING COLLECTION', (120, 200), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv.LINE_AA)
+                    cv.putText(image, 'Collection frames for {} Video Number {}'.format(action,sequence), (15, 12), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
 
-        #Break gracefully
-        if cv.waitKey(10) & 0xFF == ord('q'):
-            break
+                    #show to screen
+                    cv.imshow('Feed', image)
+                    cv.waitKey(2000)
+
+                else:
+                    cv.putText(image, 'Collection frames for {} Video Number {}'.format(action,sequence), (15, 12), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv.LINE_AA)
+                    cv.imshow('Feed', image)
+
+                #Save keypoints as numpy array for future use
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+
+                #Break gracefully
+                if cv.waitKey(10) & 0xFF == ord('q'):
+                    break
 
     capture.release()
     cv.destroyAllWindows()
